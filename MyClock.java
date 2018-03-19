0a003f71d0b66785ab119c4871167bae6e860990
DCeronio <36456323+DCeronio@users.noreply.github.com>
2018-03-19 15:40:36 -0700

    Create MyClock.java

---
+++ MyClock.java
@@ -0,0 +1,11 @@
+
+/**
+ * Aggregates both ClockFace and 3 instances of ClockHand (hour,minute, second)
+ * Make sure to use appropiate color and thickness think about what you can save in ClockHand
+ * to acheive this
+ * @author Dylan
+ *
+ */
+public class MyClock {
+
+}
