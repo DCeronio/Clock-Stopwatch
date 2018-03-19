4b1ba95977a15aec2f162dd89bb79dc09bb1298a
DCeronio <36456323+DCeronio@users.noreply.github.com>
2018-03-19 15:44:16 -0700

    Create StopWatch.java

---
+++ StopWatch.java
@@ -0,0 +1,12 @@
+
+/**
+ * Has two dials, one for outer and one for inner dials. Should show hands, May
+ * adjust the center of the two dials so that it is offset as shown
+ * 
+ * @author Dylan
+ *
+ */
+public class StopWatch {
+	
+
+}
