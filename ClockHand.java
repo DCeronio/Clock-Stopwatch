291eaf268b9baf451111293abb52b1052cc13679
DCeronio <36456323+DCeronio@users.noreply.github.com>
2018-03-19 15:41:10 -0700

    Create ClockHand.java

---
+++ ClockHand.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+/**
+ * Used to draw the hands, minute,second,hour, etc.
+ * Uses timer code from chapter 4 to move it.
+ * Need to calculate the angle to move (ClockFace can give you idea)
+ * Stroke interface to change the style of hands.
+ * @author Dylan
+ *
+ */
+public class ClockHand implements MoveableShape, Stroke {
+
+	@Override
+	public Shape createStrokedShape(Shape arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void draw(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void translate(int dx, int dy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
